6f673a91f5da875c6da1797cf266dad9e29da992
bn <benoit.nennig@supmeca.fr>
2020-05-08 15:34:15 +0200

    Remove package_data from setup.

--- setup.py
+++ setup.py
@@ -52,9 +52,6 @@ setup(
     zip_safe=False,
     packages=['amc2moodle'],
     include_package_data=True,
-    package_data={'': ['amc2moodle/xslt/*.xslt',
-                       'automultiplechoice.sty.ltxml',
-                       'test/*']},
     scripts=['amc2moodle/bin/amc2moodle'],
     classifiers=[
         "Programming Language :: Python :: 3",
