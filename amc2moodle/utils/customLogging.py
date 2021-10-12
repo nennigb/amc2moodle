d504ac3c8b79d1e908b19ea3d1148edaee950399
bn <benoit.nennig@isae-supmeca.fr>
2021-10-12 22:23:38 +0200

    Add custom logging class with counter.

--- amc2moodle/utils/customLogging.py
+++ amc2moodle/utils/customLogging.py
@@ -9,6 +9,43 @@ DEFAULT_LOG_VERBC_FORMATTER = '%(relativeCreated)dms - (%(name)s) [%(levelname)-
 DEFAULT_FMT_TIME = '%H:%M:%S'
 
 
+class CountLogger(logging.getLoggerClass()):
+    """ Create a new logger class that count warnings and errors.
+
+    The counter is incrrease each time a warning or error are logged. In all
+    cases the super methods are used.
+    The `counter` is stored has a dictionnary. Use `counter['warning']` to
+    get the value.
+    """
+
+    def __init__(self, name):
+        """ Overload base class init.
+        """
+        super().__init__(name)
+        # store counters in a dict
+        self.counter = {'warning': 0, 'error': 0, 'critical': 0}
+
+    def warning(self, msg, *args, **kwargs):
+        self.counter['warning'] += 1
+        super().warning(msg, *args, **kwargs)
+
+    def error(self, msg, *args, **kwargs):
+        self.counter['error'] += 1
+        super().error(msg, *args, **kwargs)
+
+    def critical(self, msg, *args, **kwargs):
+        self.counter['critical'] += 1
+        super().critical(msg, *args, **kwargs)
+
+    def reset_counter(self):
+        """ Manually reset the counter.
+
+        As the logger object are instanciated once. If the conversion is call
+        serveral time the counter need to be reset.
+        """
+        self.counter.update({key: 0 for key in self.counter})
+
+
 class customLogger:
     """ Basic custom Logger management.
 
@@ -17,20 +54,26 @@ class customLogger:
     """
 
     def __init__(self, loggerName='custom'):
-        """ Load logger with loggerName ID and set level to DEBUG
+        """ Load logger with loggerName ID and set level to DEBUG.
         """
+        logging.setLoggerClass(CountLogger)
         self.rootLogger = logging.getLogger(loggerName)
         self.rootLogger.setLevel(logging.DEBUG)
+        # Manually reset all logger counters at each run
+        self.rootLogger.reset_counter()
+        for log_name, log_obj in logging.Logger.manager.loggerDict.items():
+            if 'amc2moodle' in log_name:
+                logging.getLogger(log_name).reset_counter()
 
     def removeConsoleLogger(self):
-        """ Remove all console Loggers
+        """ Remove all console Loggers.
         """
         for hdlr in self.rootLogger.handlers[:]:
             if isinstance(hdlr, logging.StreamHandler):
                 self.rootLogger.removeHandler(hdlr)
 
     def removeFileLogger(self):
-        """ Remove all file loggers
+        """ Remove all file loggers.
         """
         for hdlr in self.rootLogger.handlers[:]:
             if isinstance(hdlr, logging.FileHandler):
@@ -57,9 +100,8 @@ class customLogger:
 
         if verbositylevel == 0:
             # Basic verbosity (no DEBUG, defaut)
-            sH.setFormatter(logging.Formatter(
-                fmt=DEFAULT_LOG_BASIC_FORMATTER
-            ))
+            sH.setFormatter(
+                logging.Formatter(fmt=DEFAULT_LOG_BASIC_FORMATTER))
             sH.setLevel(basicLogLevel)
         elif verbositylevel == 1:
             # First level of verbosity (classic DEBUG level)
