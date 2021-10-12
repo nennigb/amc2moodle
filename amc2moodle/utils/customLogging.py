188df552357573f589ef5985ebf6a2072af8a2ff
bn <benoit.nennig@isae-supmeca.fr>
2021-10-12 21:54:03 +0200

    Lint `customLogging.py`.

--- amc2moodle/utils/customLogging.py
+++ amc2moodle/utils/customLogging.py
@@ -4,119 +4,111 @@ import sys
 # DEFAULT
 DEFAULT_LOG_BASIC_FORMATTER = '[%(levelname)-8s]: %(message)s '
 DEFAULT_LOG_VERBA_FORMATTER = '%(relativeCreated)dms [%(levelname)-8s]: %(message)s '
-DEFAULT_LOG_VERBB_FORMATTER = lambda x : '%(relativeCreated)dms - ({}-%(name)s) [%(levelname)-8s]: %(message)s '.format(x)
+DEFAULT_LOG_VERBB_FORMATTER = lambda x: '%(relativeCreated)dms - ({}-%(name)s) [%(levelname)-8s]: %(message)s '.format(x)
 DEFAULT_LOG_VERBC_FORMATTER = '%(relativeCreated)dms - (%(name)s) [%(levelname)-8s]: %(message)s '
 DEFAULT_FMT_TIME = '%H:%M:%S'
 
 
 class customLogger:
-    """ 
-    Basic custom Logger management (load logger, remove consol/file logger, create console/file logger)
+    """ Basic custom Logger management.
+
+    It allows to load logger, remove console/file logger, create console/file
+    logger.
     """
 
-    def __init__(self,
-        loggerName='custom'):
-        """
-        Load logger with loggerName ID and set level to DEBUG
+    def __init__(self, loggerName='custom'):
+        """ Load logger with loggerName ID and set level to DEBUG
         """
         self.rootLogger = logging.getLogger(loggerName)
         self.rootLogger.setLevel(logging.DEBUG)
 
     def removeConsoleLogger(self):
-        """
-        Remove all console Loggers
+        """ Remove all console Loggers
         """
         for hdlr in self.rootLogger.handlers[:]:
-            if isinstance(hdlr,logging.StreamHandler):
+            if isinstance(hdlr, logging.StreamHandler):
                 self.rootLogger.removeHandler(hdlr)
-    
+
     def removeFileLogger(self):
-        """
-        Remove all file loggers
+        """ Remove all file loggers
         """
         for hdlr in self.rootLogger.handlers[:]:
-            if isinstance(hdlr,logging.FileHandler):
-                self.rootLogger.removeHandler(hdlr) 
+            if isinstance(hdlr, logging.FileHandler):
+                self.rootLogger.removeHandler(hdlr)
 
+    def setupConsoleLogger(self, verbositylevel=0, silent=False, txtinfo=None):
+        """ Create a console Logger.
 
-    def setupConsoleLogger(self,
-        verbositylevel = 0,
-        silent = False,
-        txtinfo = None):
+        It defines the
+        - level of verbosity (`verbositylevel` from 0 to 2)
+        - `silent` mode (overrides verbosity)
+        - a specific word in the log message (using `txtinfo`)
         """
-        Create a console Logger with
-        - level of verbosity (verbositylevel from 0 to 2)
-        - silent mode (overrides verbosity)
-        - a specific word in the log message (using txtinfo)
-        """
-        #remove all console Logger
+        # Remove all console Logger
         self.removeConsoleLogger()
-        #create Stream handler
+        # Create Stream handler
         sH = logging.StreamHandler(stream=sys.stdout)
-        #create basic logging level
+        # Create basic logging level
         basicLogLevel = logging.INFO
-        #silent mode overrides verbosity level
+        # Silent mode overrides verbosity level
         if silent:
             verbositylevel = 0
             basicLogLevel = logging.ERROR
-        #
+
         if verbositylevel == 0:
-            #basic verbosity (no DEBUG, defaut)
+            # Basic verbosity (no DEBUG, defaut)
             sH.setFormatter(logging.Formatter(
                 fmt=DEFAULT_LOG_BASIC_FORMATTER
             ))
             sH.setLevel(basicLogLevel)
         elif verbositylevel == 1:
-            #first level of verbosity (classic DEBUG level)
+            # First level of verbosity (classic DEBUG level)
             sH.setLevel(logging.DEBUG)
             sH.setFormatter(
                 logging.Formatter(fmt=DEFAULT_LOG_VERBA_FORMATTER))
         else:
-            #second level of verbosity (DEBUG level with details)
+            # Second level of verbosity (DEBUG level with details)
             sH.setLevel(logging.DEBUG)
             if txtinfo is not None:
                 fmtCustom = DEFAULT_LOG_VERBB_FORMATTER(txtinfo)
             else:
                 fmtCustom = DEFAULT_LOG_VERBC_FORMATTER
             sH.setFormatter(logging.Formatter(fmt=fmtCustom))
-        # add handler to the logger
-        self.rootLogger.addHandler(sH)           
+        # Add handler to the logger
+        self.rootLogger.addHandler(sH)
 
+    def setupFileLogger(self, filename, verbositylevel=2, silent=False,
+                        txtinfo=None):
+        """ Create a file Logger.
 
-    def setupFileLogger(self,
-        filename,
-        verbositylevel = 2,
-        silent = False,
-        txtinfo = None):
+        The new file logger is created with
+        - the file specified by `filename`
+        - level of verbosity (`verbositylevel` from 0 to 2)
+        - `silent` mode (overrides verbosity)
+        - a specific word in the log message (using `txtinfo`)
         """
-        Create a file Logger with
-        - the file specified by filename
-        - level of verbosity (verbositylevel from 0 to 2)
-        - silent mode (overrides verbosity)
-        - a specific word in the log message (using txtinfo)
-        """
-        #remove all file Loggers
+        # Remove all file Loggers
         self.removeFileLogger()
-        #create basic logging level
+        # Create basic logging level
         basicLogLevel = logging.INFO
-        #silent mode overrides verbosity level
+        # Silent mode overrides verbosity level
         if silent:
             verbositylevel = 0
             basicLogLevel = logging.ERROR
-        #create file handler
+        # Create file handler
         fH = logging.FileHandler(filename)
         if verbositylevel == 0:
-            #basic verbosity (no DEBUG)
+            # Basic verbosity (no DEBUG)
             fH.setFormatter(logging.Formatter(
                 fmt=DEFAULT_LOG_BASIC_FORMATTER
             ))
             fH.setLevel(basicLogLevel)
         elif verbositylevel == 1:
-            #first level of verbosity (classic DEBUG level)
+            # First level of verbosity (classic DEBUG level)
             fH.setLevel(logging.DEBUG)
             fH.setFormatter(logging.Formatter(fmt=DEFAULT_LOG_VERBA_FORMATTER))
         else:
-            #second level of verbosity (DEBUG level with details, default)
+            # Second level of verbosity (DEBUG level with details, default)
             fH.setLevel(logging.DEBUG)
             if txtinfo is not None:
                 fmtCustom = DEFAULT_LOG_VERBB_FORMATTER(txtinfo)
@@ -127,9 +119,6 @@ class customLogger:
         self.rootLogger.addHandler(fH)
 
     def getLogger(self):
-        """
-        Return root Logger
+        """ Return root Logger.
         """
         return self.rootLogger
-
-
