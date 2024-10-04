8aa67d2990fe4610e858f4d366f16275e02203b8
Luc LAURENT <luc.laurent@cnam.fr>
2024-10-04 22:33:18 +0200

    add test folder as submodule + decalre scripts in pyproject.toml (and rename bin's scripts)

---
+++ test/__init__.py
@@ -0,0 +1,32 @@
+"""
+    This file is part of amc2moodle, a convertion tool to recast quiz written
+    with the LaTeX format used by automuliplechoice 1.0.3 into the
+    moodle XML quiz format.
+    Copyright (C) 2016  Benoit Nennig, benoit.nennig@supmeca.fr
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+# import glob
+# import unittest
+# import os 
+
+# # run all tests
+# test_files = glob.glob(os.path.abspath(os.path.join(os.getcwd(),'test_*.py')))
+# module_strings = [test_file[0:len(test_file)-3] for test_file in test_files]
+# suites = [unittest.defaultTestLoader.loadTestsFromName(test_file) for test_file in module_strings]
+# test_suite = unittest.TestSuite(suites)
+
+# # Run autonomous
+# if __name__ == '__main__':
+#     test_runner = unittest.TextTestRunner().run(test_suite)
